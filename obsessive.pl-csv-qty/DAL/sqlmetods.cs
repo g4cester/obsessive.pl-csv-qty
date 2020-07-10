9d0ba743bc35df34c7e66e3f9a8092152a1d7eb4
Tomasz Kilian <t.kilian@speimex.com.pl>
2020-07-11 00:28:44 +0200

    sql metods

--- obsessive.pl-csv-qty/DAL/sqlmetods.cs
+++ obsessive.pl-csv-qty/DAL/sqlmetods.cs
@@ -8,7 +8,7 @@ using obsessive.pl_csv_qty.DAL;
 namespace obsessive.pl_csv_qty.DAL
 {
 
-   public class sqlmetods
+    public class sqlmetods
     {
         public virtual bool Insert(SqlEntity e)
         {
@@ -24,8 +24,39 @@ namespace obsessive.pl_csv_qty.DAL
             return ent.SaveChanges() > 0;
 
         }
+        public virtual bool UpdateQty(int id, int ilosc)
+        {
+            CDN_demoEntities ent = new CDN_demoEntities();
+            ZZSPE_TWRXML_OBS plcRecord = ent.ZZSPE_TWRXML_OBS.Where(e => e.TWRXML_ID == id).First();
+            plcRecord.TWRXML_ILOSC = ilosc;
+
+            return ent.SaveChanges() > 0;
 
 
+        }
+
+        public virtual List<SqlEntity> CheckTwrID(string twr_ean)
+
+        {
+            CDN_demoEntities ent = new CDN_demoEntities();
+            List<SqlEntity> outcome = new List<SqlEntity>();
+            List<ZZSPE_TWRXML_OBS> result = ent.ZZSPE_TWRXML_OBS.Where(e => e.TWRXML_TWR_EAN == twr_ean).ToList();
+            foreach (ZZSPE_TWRXML_OBS item in result)
+            {
+                SqlEntity sqte = new SqlEntity();
+                sqte.TWRXML_ID = item.TWRXML_ID;
+                sqte.TWRXML_TWR_EAN = item.TWRXML_TWR_EAN;
+                sqte.TWRXML_ILOSC = item.TWRXML_ILOSC.Value;
+
+                outcome.Add(sqte);
+
+            }
+
+            return outcome;
+
+
+
+
+        }
     }
-
 }
